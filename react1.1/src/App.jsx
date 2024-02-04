4b57e44036226dd43a117380624c0432597e2e94
blaz3fox <kuldeeppawar427@gmail.com>
2024-02-04 22:26:05 +0530

    react1

---
+++ react1.1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+//JSX is a coding standard that allows users to use JavaScript expressions and other HTML features inline. 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+        <button onClick={()=> setCount((count)=>count +1)} >
+          count is {count}
+          {/* updates the state variable react handles with updation of the dom */}
+        </button>
+    
+      </div>
+     
+    
+  )
+}
+
+export default App
