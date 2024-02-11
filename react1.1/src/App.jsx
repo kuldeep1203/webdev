8779829dd25318ca64309d64b964ef2eaebff84c
blaz3fox <kuldeeppawar427@gmail.com>
2024-02-11 14:49:01 +0530

    week5

--- react1.1/src/App.jsx
+++ react1.1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 //JSX is a coding standard that allows users to use JavaScript expressions and other HTML features inline. 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)//we need to define it like this for react to watch it 
 
   return (
     
