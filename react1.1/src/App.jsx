197a77eea9c329df067a018222c169ee5bdec423
blaz3fox <kuldeeppawar427@gmail.com>
2024-02-14 15:45:43 +0530

    save

--- react1.1/src/App.jsx
+++ react1.1/src/App.jsx
@@ -1,21 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 //JSX is a coding standard that allows users to use JavaScript expressions and other HTML features inline. 
-function App() {
-  const [count, setCount] = useState(0)//we need to define it like this for react to watch it 
+// //to do{
+//   todos:[{title:,desc:,completed:}]
+// }
 
+function App() {
+  const [todos, setTodos] = useState([{
+    title:"go to gym",
+    description:"from 6- 9",
+    completed: false,
+  }, {
+    title:"study dsa",
+    description:"from 9-12",
+    completed: false,
+  }])
+  
+  //we need to define it like this for react to watch it 
+  //usestate hook is used to define a state so react can watch the variable
   return (
     
       <div>
-        <button onClick={()=> setCount((count)=>count +1)} >
-          count is {count}
+        
+          {/* we write js components in curly brace so the above thing is a function definded in curly braces  */}
           {/* updates the state variable react handles with updation of the dom */}
-        </button>
+          {
+            todos.map(function(todo){
+              return <Todo title={todo.title} description = {todo.description} />
+            })
+          }
+          
+        
     
       </div>
      
     
   )
+}
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
---
+++ todo/readme.md
@@ -0,0 +1,7 @@
+## TO do app  
+this project consist of a very simple todo application 
+features included :
+    -can create a todo 
+    -can see their exisiting todo list
+    -anyone can mark to do as done
+    
