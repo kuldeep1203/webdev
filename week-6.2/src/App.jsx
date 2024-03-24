f78696976476593eac33e72d6ee3e462b25fb0a4
kuldeep <kuldeeppaawar427@gmail.com>
2024-03-24 11:56:52 +0530

    week5-test

--- week-6.2/src/App.jsx
+++ week-6.2/src/App.jsx
@@ -1,58 +1,33 @@
-import { useState} from 'react';
-import {useEffect} from 'react';
-
-import './App.css'
-
-
-//useeffect has a function to run and a dependency array 
-//sum-server.100xdevs.com/todos
-
-//Some components need to synchronize with external systems.
-// For example, you might want to control a non-React component 
-//based on the React state, set up a server connection, or send an analytics log when a component appears on the screen. 
-//Effects let you run some code after rendering so that you can synchronize your component with some system outside of React.
-
+import React, { useState, memo } from "react";
+import { useCallback } from "react";
 
 function App() {
-  const [todos,setTodos] = useState([]);
-  useEffect(()=>{
-    setInterval(()=>{
-      fetch("https://sum-server.100xdevs.com/todos")
-      .then(async (res)=>{
-        const json =await res.json();
-        setTodos(json.todos);
-      })
-    },1000)
-    
+  const [counter, setCounter] = useState(0);
+
+  const logSomething=useCallback(() => { 
+    console.log("child clicked");
   },[]);
 
-  return <div>
-      {todos.map(todo => (
-        <Todo key={todo.id} title={todo.title} description={todo.description}/>
-      ))}
-      
-    </div>
-  
-  
-}
-function Todo({title, description}){
   return (
-  <div>
-    <h1>
-      {title}
-    </h1>
-    <h5>
-      {description}
-    </h5>
-  </div>
-  )
+    <div>
+      <ButtonComponent inputFunction={logSomething} />
+      <button onClick={() => {
+        setCounter(counter + 1);
+      }}>Click me {counter}</button>
+    </div>
+  );
 }
 
-// function CardWrapper({children}){
-//   return <div style={{border : "2px solid black"}}>
-//     {children}
-//   </div>
-// }
+const ButtonComponent = memo(({ inputFunction }) => {
+  console.log("child render");
 
-export default App
-//hooks allow u to hook into thereact state and lifecylce features from fucntion components
+  return (
+    <div>
+      <button onClick={inputFunction}>Button Clicked</button>
+    </div>
+  );
+});
+
+export default App;
+
+
