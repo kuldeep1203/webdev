a2a36def4c7d1ed8758d9d00b6c888408b8f14a4
kuldeep <kuldeeppaawar427@gmail.com>
2024-04-11 15:50:16 +0530

    recoil

--- week7.1.2/src/App.jsx
+++ week7.1.2/src/App.jsx
@@ -31,6 +31,14 @@ function Count({setCount}){//count doesnt really need it but button componenet d
     
     <Buttons setCount={setCount}/>
   </div>
+  //count would rerender even though we are not using count variable here 
+  //context api does ahve this drawback  
+  //so why do we exactly use context api ??? - we dont use it to make rendering more performant 
+  //only reason we use it for making the syntax cleaner and get rid of prop drilling
+  //to combat the issue of rerendering we use state management libraries 
+  //statemanagement  provides a cleaner way to store the state of your app
+  // recoil,redux,zustand - helps to divide project into state and components
+  // Recoil 
 }
 
 
