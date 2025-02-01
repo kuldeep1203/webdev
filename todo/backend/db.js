08fb53e5b7c5e65d5f9e52e8941fecb851c40859
kuldeep <89348139+kuldeep1203@users.noreply.github.com>
2025-02-01 09:55:32 +0530

    Update db.js

--- todo/backend/db.js
+++ todo/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kuldeep:simon@cluster0.ygqebfp.mongodb.net/todos").then(() => console.log('Connected!'));
+mongoose.connect("").then(() => console.log('Connected!'));
 
 const todoSchema = mongoose.Schema({
     title: String,
